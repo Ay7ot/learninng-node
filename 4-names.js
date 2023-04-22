67489a2dbcfaf2a0a2f2c73f76bf327d9b936eba
Aydot <ayomidotzee@gmail.com>
2023-04-22 07:14:52 -0700

    April 22 1

---
+++ 4-names.js
@@ -0,0 +1,11 @@
+//sharing
+const john = 'john';
+const peter = 'peter';
+
+//local
+const SECRET = 'SUPER SECRET'
+
+module.exports = {
+    john,
+    peter
+}
---
+++ app.js
@@ -0,0 +1,11 @@
+// Modules
+
+const { john, peter } = require('./4-names');
+const {sayHi} = require('./5-utils');
+const { singlePerson, items } = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+
+sayHi(john);
+sayHi(peter);
+sayHi('susan');
+console.log( singlePerson, items )
