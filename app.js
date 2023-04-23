5a13ecbe782f0c594b5cbcf3090785e5393d33e2
Aydot <ayomidotzee@gmail.com>
2023-04-23 02:48:22 -0700

    April 23 1

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const { readFileSync, writeFileSync } = require('fs')
 
-const first = readFileSync('./content/first.txt', 'utf8');
-const second = readFileSync('./content/second.txt', 'utf8');
 
-console.log(first, second);
+const _ = require('lodash')
 
-writeFileSync('./content/result-sync.txt', `Here is the result : ${first}, ${second}`)
+const items = [1, [2, [3, [4]]]]
 
+const newItems = _.flattenDeep(items)
+
+console.log(newItems)
